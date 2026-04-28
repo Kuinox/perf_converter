830f364a2aa39d0f9fe0e445dee615df54c0addb
Kuinox <nvandeginste@gmail.com>
2026-04-28 14:03:34 +0200

    Use older attribute array syntax in temp schema

--- Temp.Schema/Schema/StackRangeRowSchema.cs
+++ Temp.Schema/Schema/StackRangeRowSchema.cs
@@ -5,9 +5,9 @@ namespace Temp.Schema.Schema;
 [ParquetSchema]
 public sealed partial class StackRangeRowSchema
 {
-    [ParquetColumn("startTrace", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("startTrace", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong StartTrace { get; set; }
 
-    [ParquetColumn("endTrace", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("endTrace", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong EndTrace { get; set; }
 }
--- Temp.Schema/Schema/TraceSampleRowSchema.cs
+++ Temp.Schema/Schema/TraceSampleRowSchema.cs
@@ -5,132 +5,132 @@ namespace Temp.Schema.Schema;
 [ParquetSchema]
 public sealed partial class TraceSampleRowSchema
 {
-    [ParquetColumn("id", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("id", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong Id { get; set; }
 
-    [ParquetColumn("perfId", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("perfId", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong PerfId { get; set; }
 
-    [ParquetColumn("pid", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("pid", Encodings = new[] { EncodingKind.RleDictionary })]
     public uint Pid { get; set; }
 
-    [ParquetColumn("tid", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("tid", Encodings = new[] { EncodingKind.RleDictionary })]
     public uint Tid { get; set; }
 
-    [ParquetColumn("time", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("time", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong Time { get; set; }
 
-    [ParquetColumn("cpu", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("cpu", Encodings = new[] { EncodingKind.RleDictionary })]
     public uint Cpu { get; set; }
 
-    [ParquetColumn("flags", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("flags", Encodings = new[] { EncodingKind.RleDictionary })]
     public uint Flags { get; set; }
 
-    [ParquetColumn("ip", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("ip", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong Ip { get; set; }
 
-    [ParquetColumn("addr", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("addr", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong Addr { get; set; }
 
-    [ParquetColumn("period", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("period", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong Period { get; set; }
 
-    [ParquetColumn("insnCnt", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("insnCnt", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong InsnCnt { get; set; }
 
-    [ParquetColumn("cycCnt", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("cycCnt", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong CycCnt { get; set; }
 
-    [ParquetColumn("weight", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("weight", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong Weight { get; set; }
 
-    [ParquetColumn("cpumode", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("cpumode", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte Cpumode { get; set; }
 
-    [ParquetColumn("addrCorrelatesSym", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("addrCorrelatesSym", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte AddrCorrelatesSym { get; set; }
 
-    [ParquetColumn("event", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("event", Encodings = new[] { EncodingKind.RleDictionary })]
     public ReadOnlyMemory<byte> Event { get; set; }
 
-    [ParquetColumn("machinePid", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("machinePid", Encodings = new[] { EncodingKind.RleDictionary })]
     public uint MachinePid { get; set; }
 
-    [ParquetColumn("vcpu", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("vcpu", Encodings = new[] { EncodingKind.RleDictionary })]
     public uint Vcpu { get; set; }
 
-    [ParquetColumn("srcFileName", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("srcFileName", Encodings = new[] { EncodingKind.RleDictionary })]
     public ReadOnlyMemory<byte>? SourceFileName { get; set; }
 
-    [ParquetColumn("srcLineNumber", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("srcLineNumber", Encodings = new[] { EncodingKind.RleDictionary })]
     public uint SourceLineNumber { get; set; }
 
-    [ParquetColumn("ipSymoff", Encodings = [EncodingKind.Plain])]
+    [ParquetColumn("ipSymoff", Encodings = new[] { EncodingKind.Plain })]
     public uint IpSymoff { get; set; }
 
-    [ParquetColumn("ipSym", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("ipSym", Encodings = new[] { EncodingKind.RleDictionary })]
     public ReadOnlyMemory<byte>? IpSym { get; set; }
 
-    [ParquetColumn("ipSymStart", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("ipSymStart", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong IpSymStart { get; set; }
 
-    [ParquetColumn("ipSymEnd", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("ipSymEnd", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong IpSymEnd { get; set; }
 
-    [ParquetColumn("ipDso", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("ipDso", Encodings = new[] { EncodingKind.RleDictionary })]
     public ReadOnlyMemory<byte>? IpDso { get; set; }
 
-    [ParquetColumn("ipSymBinding", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("ipSymBinding", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte IpSymBinding { get; set; }
 
-    [ParquetColumn("ipIs64Bit", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("ipIs64Bit", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte IpIs64Bit { get; set; }
 
-    [ParquetColumn("ipIsKernelIp", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("ipIsKernelIp", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte IpIsKernelIp { get; set; }
 
-    [ParquetColumn("ipBuildId", Encodings = [EncodingKind.Plain])]
+    [ParquetColumn("ipBuildId", Encodings = new[] { EncodingKind.Plain })]
     public ReadOnlyMemory<byte> IpBuildId { get; set; }
 
-    [ParquetColumn("ipFiltered", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("ipFiltered", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte IpFiltered { get; set; }
 
-    [ParquetColumn("ipComm", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("ipComm", Encodings = new[] { EncodingKind.RleDictionary })]
     public ReadOnlyMemory<byte>? IpComm { get; set; }
 
-    [ParquetColumn("haveAddress", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("haveAddress", Encodings = new[] { EncodingKind.RleDictionary })]
     public bool HaveAddress { get; set; }
 
-    [ParquetColumn("addressSymoff", Encodings = [EncodingKind.Plain])]
+    [ParquetColumn("addressSymoff", Encodings = new[] { EncodingKind.Plain })]
     public uint AddressSymoff { get; set; }
 
-    [ParquetColumn("addressSym", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("addressSym", Encodings = new[] { EncodingKind.RleDictionary })]
     public ReadOnlyMemory<byte>? AddressSym { get; set; }
 
-    [ParquetColumn("addressSymStart", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("addressSymStart", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong AddressSymStart { get; set; }
 
-    [ParquetColumn("addressSymEnd", Encodings = [EncodingKind.DeltaBinaryPacked])]
+    [ParquetColumn("addressSymEnd", Encodings = new[] { EncodingKind.DeltaBinaryPacked })]
     public ulong AddressSymEnd { get; set; }
 
-    [ParquetColumn("addressDso", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("addressDso", Encodings = new[] { EncodingKind.RleDictionary })]
     public ReadOnlyMemory<byte>? AddressDso { get; set; }
 
-    [ParquetColumn("addressSymBinding", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("addressSymBinding", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte AddressSymBinding { get; set; }
 
-    [ParquetColumn("addressIs64Bit", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("addressIs64Bit", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte AddressIs64Bit { get; set; }
 
-    [ParquetColumn("addressIsKernelIp", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("addressIsKernelIp", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte AddressIsKernelIp { get; set; }
 
-    [ParquetColumn("addressBuildId", Encodings = [EncodingKind.Plain])]
+    [ParquetColumn("addressBuildId", Encodings = new[] { EncodingKind.Plain })]
     public ReadOnlyMemory<byte>? AddressBuildId { get; set; }
 
-    [ParquetColumn("addressFiltered", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("addressFiltered", Encodings = new[] { EncodingKind.RleDictionary })]
     public byte AddressFiltered { get; set; }
 
-    [ParquetColumn("addressComm", Encodings = [EncodingKind.RleDictionary])]
+    [ParquetColumn("addressComm", Encodings = new[] { EncodingKind.RleDictionary })]
     public ReadOnlyMemory<byte>? AddressComm { get; set; }
 }
